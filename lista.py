dc3acb1ae250205e0297c6264742342e7f0f4a1b
nicolas <nicobilbao29il@gmail.com>
2025-03-03 16:45:51 -0300

    se agrego la funcion lista

---
+++ lista.py
@@ -0,0 +1,18 @@
+# Crear una lista
+frutas = ['manzana', 'plátano', 'cereza', 'naranja']
+
+# Acceder a elementos de la lista (por índice)
+print(frutas[0])  # manzana
+print(frutas[2])  # cereza
+
+# Cambiar un valor en la lista
+frutas[1] = 'kiwi'
+
+# Agregar un nuevo elemento al final de la lista
+frutas.append('mango')
+
+# Eliminar un elemento de la lista
+frutas.remove('naranja')
+
+# Imprimir la lista modificada
+print(frutas)  # ['manzana', 'kiwi', 'cereza', 'mango']
