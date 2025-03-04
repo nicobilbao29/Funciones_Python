a162dd9c82e5996406f537ad6f8466fb77d21175
Nicolas <nicobilbao29@gmail.com>
2025-03-03 22:29:37 -0300

    Subir solo la carpeta condicionales/

---
+++ condicionales/condicional_if.py
@@ -0,0 +1,8 @@
+# Solicitar al usuario que ingrese su edad
+edad = int(input("Por favor, ingresa tu edad: "))
+
+# Condicional para verificar si el usuario es mayor de edad
+if edad >= 18:
+    print("Eres mayor de edad.")
+else:
+    print("Eres menor de edad.")
